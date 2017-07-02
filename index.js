e4b73b90c8b46166d5d996679670054a26858594
jnhustin <jnhustin@gmail.com>
2017-07-02 16:07:47 -0700

    added route for single character info

--- index.js
+++ index.js
@@ -40,7 +40,24 @@ app.get('/account', function(req, res) {
   return deferred.promise;
 });
 
-
+// get single char info
+app.get('/character', function(req, res) {
+  const deferred  = q.defer();
+  let battleTag   = req.query.tag;
+  let charName    = req.query.charId;
+  let reqParams   = requestParams;
+  reqParams.url   = 'https://us.api.battle.net/d3/profile/' + battleTag + '/hero/' + charId
+  
+  rp(reqParams)
+  .then(function(data){
+    console.log(data);
+    deferred.resolve(data);
+  })
+  .catch(function(err){
+    console.log('error in get single char',err);
+    deferred.reject(err);
+  });
+});
 
 const port = process.env.PORT || 9001;
 
