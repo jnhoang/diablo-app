3db1960532f3c4879458ab246b327e58b75d867b
jnhustin <jnhustin@gmail.com>
2017-07-02 16:04:24 -0700

    switched to request-promise, abstracted request params

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 require('dotenv').config();
 const express   = require('express');
+const request   = require('request');
+const rp        = require('request-promise');
+const q         = require('q');
 
 const app = express();
+const search = 'Toothonius-1398'
+
+const requestParams = {
+  method: 'GET'
+, url: ''
+, json: true
+, qs: {
+    
+    // EVENTUALLY CHANGE LOCALE (should default to US)
+    locale: 'en_US'
+  , apikey: process.env.DIABLO_KEY
+  }
+}
+
+
+// get account info
+app.get('/account', function(req, res) {
+  const deferred  = q.defer();
+  let battleTag   = req.query.tag;
+  let reqParams   = requestParams;
+  reqParams.url   = 'https://us.api.battle.net/d3/profile/' + battleTag + '/';
+  
+  rp(reqParams)
+  .then(function(data) {
+    console.log(data);
+    deferred.resolve(data);
+  })
+  .catch(function(err) {
+    console.log('error in get account info');
+    deferred.reject(err);
+  })
+  
+  return deferred.promise;
+});
 
 
 
