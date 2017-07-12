1cf047c688276b4f743e726eeee4c53b75c7ba64
jnhustin <jnhustin@gmail.com>
2017-07-12 16:31:36 -0700

    refactor hero component

--- client/src/home/components/Hero.jsx
+++ client/src/home/components/Hero.jsx
@@ -2,14 +2,17 @@ import React  from 'react';
 import {Link} from 'react-router-dom';
 
 function Hero(props) {
-  let hero        = props.hero;
-  let battleTag   = props.acct.replace('#', '-');
-  let gender = hero.gender === 0 ? "male" : "female";
+  let hero            = props.hero;
+  let battleTag       = props.acct.replace('#', '-');
+  let gender          = hero.gender === 0 ? "male" : "female";
+  let backgroundLink  = `url('${process.env.PUBLIC_URL}/static/img/class-head/${hero.class}_${gender}.png')`
+  
   return (
     <div>
       <div 
         className="class-portrait"
-        style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/static/img/class-head/${hero.class}_${gender}.png')` }}></div>
+        style={{ backgroundImage: backgroundLink }}>
+      </div>
       <Link to={`/character/${ battleTag }/${ hero.id }`}>{hero.name}</Link>
       <p>class          : {hero.class}</p>
       <p>gender         : {gender}</p>
